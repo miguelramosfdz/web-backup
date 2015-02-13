5e83ce424ccc59ebe8909dfe06f077f965755f27
yieme <yieme@cormea.com>
2015-02-13 12:24:31 -0700

    initial

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# web-backup
+# web-backup <img src="https://raw.githubusercontent.com/voodootikigod/logo.js/master/js.png" align="right" height="165">
 Make a local copy of your online website
+
+## Install
+
+```js
+npm install web-backup --save
+```
+
+## License: MIT
---
+++ index.js
@@ -0,0 +1,17 @@
+// web-backup - Make a local copy of your online website - Copyright (C) 2015 by yieme - All Rights Reserved - License: MIT
+
+;(function() {
+  'use strict';
+
+  // var convar = require('convar')
+
+  var WebBackup = function() {
+
+  }
+
+
+
+  if (typeof exports === 'object') module.exports = WebBackup // support CommonJS
+  // else if (typeof define === 'function' && define.amd) define(function() { return WebBackup }) // support AMD
+  // else this.WebBackup = WebBackup // support browser
+})();
